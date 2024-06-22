1c141cd09f1726418788757c0986f5047bd26bb8
tikkhun <s565338107@163.com>
2024-06-22 11:49:15 +0800

    chore: 随机数。

--- packages/utils/lib/crypt.test.ts
+++ packages/utils/lib/crypt.test.ts
@@ -3,21 +3,20 @@ import { SimpleCrypto } from "../lib/crypt";
 import CryptoJS from "crypto-js";
 it("simple crypto", () => {
   const crypto = new SimpleCrypto("tikkhbodyencryptdecryptpassword1");
-  const original = "aaa";
+  const original = "" + Math.random();
   const dtext = crypto.aesEncrypt(original);
-  console.log(`dtext`, dtext);
   const text = crypto.aesDecrypt(dtext);
-  console.log(`text`, text);
   expect(text).toBe(original);
 });
 
 it("crypto", () => {
   // Encrypt
-  const original = "my message";
-  var ciphertext = CryptoJS.AES.encrypt(original, "secret key 123").toString();
+  const original = "" + Math.random();
+  const password = "" + Math.random();
+  var ciphertext = CryptoJS.AES.encrypt(original, password).toString();
 
   // Decrypt
-  var bytes = CryptoJS.AES.decrypt(ciphertext, "secret key 123");
+  var bytes = CryptoJS.AES.decrypt(ciphertext, password);
   var originalText = bytes.toString(CryptoJS.enc.Utf8);
 
   expect(originalText).toBe(original);
